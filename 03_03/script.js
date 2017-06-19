c18e228d63071e875e4bb671dc58434f5f22a64b
Christopher Justice <justcd@mail.broward.edu>
2017-06-19 15:55:34 -0400

    operators standard notes

---
+++ 03_03/script.js
@@ -0,0 +1,19 @@
+/* operators
+	- = is used for assinment
+	-  + for addition
+	-  - for subtraction
+	-  * for multiplication
+	-  / for division
+
+Javascript follows standard algebra rules (PEMDA - Paranthesis Exponents Multiply Divide Addition)
+
+*/
+
+let negInteger = -3.14159265359;
+let escQuote = "Quotes can also be \"escaped\".";
+let theSunIsWarm = true;
+let emptyInside = null;
+let justAnotherVariable;
+
+
+console.log(typeof negInteger);
