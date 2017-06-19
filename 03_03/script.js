c21ed58aafaabbe9d479a78753bb3eb35d64e788
Christopher Justice <justcd@mail.broward.edu>
2017-06-19 15:55:50 -0400

    operators standard notes

--- 03_03/script.js
+++ 03_03/script.js
@@ -14,6 +14,9 @@ let escQuote = "Quotes can also be \"escaped\".";
 let theSunIsWarm = true;
 let emptyInside = null;
 let justAnotherVariable;
+let a = 1;
 
-
+console.log(a);
+console.log(++a); // ++ take 1 and adds to the value, then  outputs the value;
+console.log(a++); // takes a and adds 1 to it so when you check a it will be 2;
 console.log(typeof negInteger);
