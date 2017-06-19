551c9368ad01a7d8a953b9eccb3945790845711b
Christopher Justice <justcd@mail.broward.edu>
2017-06-19 16:36:16 -0400

    operators standard notes

--- 03_04/script.js
+++ 03_04/script.js
@@ -1,3 +1,24 @@
-var a = 5;
-var b = 4;
-var sum = a + b;
+let a = 5;
+let b = 4;
+let sum = a + b;
+
+let c = 4;
+let d = "5";
+let total = c + d;
+let sub = c - d;
+
+console.log(a, "this is a", typeof a);
+console.log(b, "this is b " ,typeof b);
+console.log(sum, "value of a + b ");
+// because one is a string Javascript will work as a string operator. this will return "45";
+console.log(c, "this is a", typeof c);
+console.log(d, "this is b ",typeof d);
+console.log(total, "value of c + d ", typeof total);
+
+// if you do a subtraction this will subtract. it is sloppy but will work
+
+console.log(sub, "subtraction of a number and string.");
+
+document.body.innerHTML = "<h1> Check the console </h1>"
+
+
---
+++ 03_05/script.js
@@ -0,0 +1,16 @@
+// const is used so the value cannot be changed while checking for equality. Try changing the equality to one equal sign.
+// two equal signs is not as strict. a string will be the same value ex: 5, "5" are the same with two.
+// three equal signs will not allow 5, "5". it has to match exactly.
+
+const a = 5;
+const b = 5;
+let theNumbersMatch
+
+if ( a === b ) {
+	theNumbersMatch = true;
+} else {
+    theNumbersMatch = false;
+}
+
+console.log("The numbers match: ",theNumbersMatch);
+
