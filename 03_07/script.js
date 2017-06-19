b3d60d9052bb2802a81e812cd083f2fa87f724db
Christopher Justice <justcd@mail.broward.edu>
2017-06-19 16:49:27 -0400

    array

---
+++ 03_07/script.js
@@ -0,0 +1,19 @@
+let pens;
+pens = ["red", "blue", "green", "orange"];
+console.log("Pens array is an ", typeof pens);
+console.log(pens);
+
+
+let mixedArray;
+mixedArray = [1,"string","blue",3];
+console.log("mixedArray is an ", typeof mixedArray);
+console.log(mixedArray);
+mixedArray[3] = 4;
+console.log(mixedArray, " [3] has been modified");
+
+
+
+
+
+
+document.body.innerHTML = "<h4> Check the console </h4>"
