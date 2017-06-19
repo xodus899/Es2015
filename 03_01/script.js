be1ce6112962ae7265dd6fbd7934c9593f713040
Christopher Justice <justcd@mail.broward.edu>
2017-06-18 21:44:09 -0400

    using let

---
+++ 03_01/script.js
@@ -0,0 +1,14 @@
+let num1 = 7;
+let num2 = 4;
+let sum = num1 + num2;
+
+document.body.innerHTML = " <h1> The sum is " + sum + "</h1>"
+
+// can also put variables all on one line.
+
+let fNum, sNum, total;
+	fNum = 3;
+	sNum = 4;
+	total = fNum + sNum;
+
+document.body.innerHTML = " <h1> The total is " + total + "</h1>"
