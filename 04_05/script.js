066bfff41b2b548f6877c64e0f9dfdfb89db7615
Christopher Justice <justcd@mail.broward.edu>
2017-06-26 11:05:51 -0400

    anon fun,ternary

--- 04_05/script.js
+++ 04_05/script.js
@@ -1,14 +1,45 @@
-function findBiggestFraction(a,b) {
-    var result;
-    a>b ? result = ["firstFraction", a] : result = ["secondFraction", b];
-    return result;
+let a = 5/7;
+let b = 18/25;
+let num1 = 3/4;
+let num2 = 5/5;
+
+
+// ternary
+let theBiggest = function() {
+	let result;
+	a > b ? result = ["a: ", a] : result = ["b: ", b];
+	console.log(result);
+}
+theBiggest();
+
+document.querySelector("#h4").innerHTML = "Check console";
+
+// conditional
+let theSmallest =  function() {
+	let result;
+
+	if(num1 > num2) {
+		result = ["num1: ", num1];
+	} else if (a < b) {
+		result = ["num2 is bigger: ", num2, "num1 value was: ", num1];
+	} else {
+		"Neither is bigger";
+	}
+	console.log(result);
 }
 
-var firstFraction = 3/4;
-var secondFraction = 5/7;
+theSmallest();
 
-var fractionResult = findBiggestFraction(firstFraction,secondFraction);
+// anon funtion with parameters
 
-console.log("First fraction result: ", firstFraction);
-console.log("Second fraction result: ", secondFraction);
-console.log("Fraction " + fractionResult[0] + " with a value of " + fractionResult[1] + " is the biggest!");
+let theParameters = function(num3,num4) {
+	if(num3 > num4) {
+		console.log("num3 is bigger ", num3);
+	} else if (num3 < num4) {
+		console.log("num4 is bigger ", num4);
+	} else {
+		console.log("The numbers are the same");
+	}
+}
+
+theParameters(3/4,3/5);
